324156054ac00303d38a7f7b501e7a15f115aa41
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2022-01-13 11:15:23 +0000

    fix: entry point

--- run.sh
+++ run.sh
@@ -2,11 +2,18 @@
 
 echo ' ####### start to train ######## '
 
-if [ $1 != "debug"]; then
-        python exp/exp004.py \
-                --train-fold 0 1 2 3 4 5 6 7 8 9
-else
-        python exp/exp004.py \
+if [ $1 = "debug" ]; then
+        python exp/exp005.py \
                 --debug \
-                --train-fold 0
-if
+                --train_fold 0
+fi
+
+if [ $1 = "all" ]; then
+        python exp/ex005.py \
+                --train_fold 0 1 2 3 4 5 6 7 8 9
+fi
+
+if [ $1 = "val" ]; then
+        python exp/exp005.py \
+                --train_fold 0
+fi
